184ee145fb567a8146f05e2252535c201ba25d0a
vsubotskyi <vsubotskyi@arturo.ai>
2024-09-20 18:22:32 +0300

    DEVOPSX-768: comment out OAM Sink in the Monitoring account

--- monitoring/terraform/oam-sink.tf
+++ monitoring/terraform/oam-sink.tf
@@ -1,36 +1,36 @@
-data "aws_organizations_organization" "this" {}
-
-resource "awscc_oam_sink" "this" {
-  name = "SampleSink"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = "*"
-      Resource  = "*"
-      Action    = [
-        "oam:CreateLink",
-        "oam:UpdateLink"
-      ]
-      Condition = {
-        StringEquals = {
-          "aws:PrincipalOrgID" = data.aws_organizations_organization.this.id
-        }
-        "ForAllValues:StringEquals" = {
-          "oam:ResourceTypes" = [
-            "AWS::CloudWatch::Metric",
-            "AWS::Logs::LogGroup",
-            "AWS::XRay::Trace",
-            "AWS::ApplicationInsights::Application",
-            "AWS::InternetMonitor::Monitor"
-          ]
-        }
-      }
-    }]
-  })
-}
-
-output "oam_sink" {
-  value = awscc_oam_sink.this
-}
+#data "aws_organizations_organization" "this" {}
+#
+#resource "awscc_oam_sink" "this" {
+#  name = "SampleSink"
+#
+#  policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [{
+#      Effect    = "Allow"
+#      Principal = "*"
+#      Resource  = "*"
+#      Action    = [
+#        "oam:CreateLink",
+#        "oam:UpdateLink"
+#      ]
+#      Condition = {
+#        StringEquals = {
+#          "aws:PrincipalOrgID" = data.aws_organizations_organization.this.id
+#        }
+#        "ForAllValues:StringEquals" = {
+#          "oam:ResourceTypes" = [
+#            "AWS::CloudWatch::Metric",
+#            "AWS::Logs::LogGroup",
+#            "AWS::XRay::Trace",
+#            "AWS::ApplicationInsights::Application",
+#            "AWS::InternetMonitor::Monitor"
+#          ]
+#        }
+#      }
+#    }]
+#  })
+#}
+#
+#output "oam_sink" {
+#  value = awscc_oam_sink.this
+#}
