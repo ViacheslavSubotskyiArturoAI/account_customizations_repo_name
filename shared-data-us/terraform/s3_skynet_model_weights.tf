80a270c46a5c0ca6f0a3f268a14ad2a5fb1abade
vsubotskyi <vsubotskyi@arturo.ai>
2024-10-14 21:00:30 +0300

    DEVOPSX-770: PoC Shared accounts

---
+++ shared-data-us/terraform/s3_skynet_model_weights.tf
@@ -0,0 +1,44 @@
+resource "aws_s3_bucket" "skynet_model_weights" {
+  bucket = local.s3_skynet_model_weights_bucket_name
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "skynet_model_weights" {
+  bucket = aws_s3_bucket.skynet_model_weights.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "skynet_model_weights" {
+  bucket = aws_s3_bucket.skynet_model_weights.id
+  policy = data.aws_iam_policy_document.s3_bucket_skynet_model_weights.json
+}
+
+data "aws_iam_policy_document" "s3_bucket_skynet_model_weights" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:GetLifecycleConfiguration",
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketAcl",
+      "s3:GetBucketLocation"
+    ]
+    resources = [
+      aws_s3_bucket.skynet_model_weights.arn,
+      "${aws_s3_bucket.skynet_model_weights.arn}/*",
+    ]
+    condition {
+      test     = "ForAnyValue:StringLike"
+      variable = "aws:PrincipalOrgPaths"
+      values   = local.s3_skynet_model_weights_allowed_ro_principal_org_paths
+    }
+  }
+}
--- shared-data-us/terraform/s3_skynet_model_weights_locals.tf
+++ shared-data-us/terraform/s3_skynet_model_weights_locals.tf
@@ -1,6 +1,6 @@
 locals {
-  s3_model_weights_bucket_name = "skynet-us-model-weights-${local.account_id}"
-  s3_model_weights_allowed_ro_principal_org_paths = [
+  s3_skynet_model_weights_bucket_name = "skynet-us-model-weights-${local.account_id}"
+  s3_skynet_model_weights_allowed_ro_principal_org_paths = [
     "${local.org_id}/${local.org_root_id}/${local.org_ou_prod_id}/*",
     "${local.org_id}/${local.org_root_id}/${local.org_ou_non_prod_id}/*"
   ]
