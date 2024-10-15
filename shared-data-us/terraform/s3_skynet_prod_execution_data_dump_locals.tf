0d17c4bb1b3e32d4f06bf0580ef531562ed03e00
vsubotskyi <vsubotskyi@arturo.ai>
2024-10-15 17:49:07 +0300

    DEVOPSX-770: tune PoC Shared accounts

--- shared-data-us/terraform/s3_skynet_prod_execution_data_dump_locals.tf
+++ shared-data-us/terraform/s3_skynet_prod_execution_data_dump_locals.tf
@@ -1,6 +1,6 @@
 locals {
-  s3_skynet_prod_execution_data_dump_bucket_name         = "skynet-us-prod-execution-data-dump-${local.account_id}"
-  s3_skynet_prod_execution_data_dump_replica_bucket_name = "skynet-us-prod-execution-data-dump-replica-${local.account_id}"
+  s3_skynet_prod_execution_data_dump_bucket_name         = "skynet-prod-us-execution-data-dump-${local.account_id}"
+  s3_skynet_prod_execution_data_dump_replica_bucket_name = "skynet-prod-us-execution-data-dump-replica-${local.account_id}"
   s3_skynet_prod_execution_data_dump_allowed_rw_aws_principals_identifiers = [
     "575108956402" # Test-Inference-US
   ]
