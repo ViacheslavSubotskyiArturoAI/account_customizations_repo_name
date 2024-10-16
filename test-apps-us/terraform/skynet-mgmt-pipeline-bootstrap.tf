4c22a6bb093ffb7618cd868c90542610c80cc647
vsubotskyi <vsubotskyi@arturo.ai>
2024-10-16 19:53:09 +0300

    DEVOPSX-771: use feature branch

--- test-apps-us/terraform/skynet-mgmt-pipeline-bootstrap.tf
+++ test-apps-us/terraform/skynet-mgmt-pipeline-bootstrap.tf
@@ -3,6 +3,6 @@ module "skynet_mgmt_pipeline_bootstrap" {
 
   environment                            = "test"
   environment_type                       = "lower"
-  skynet_on_demand_iac_repo_branch       = "dev"
+  skynet_on_demand_iac_repo_branch       = "DEVOPSX-771-dev"
   cloudwatch_log_group_retention_in_days = "30"
 }
