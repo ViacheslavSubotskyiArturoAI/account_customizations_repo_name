63e8ca98be01c8fa2ef0cf1adf327c143ae5e4a2
vsubotskyi <vsubotskyi@arturo.ai>
2024-10-17 11:32:48 +0300

    DEVOPSX-771: tune test env

--- _modules/skynet-inference-bootstrap/servicequotas.tf
+++ _modules/skynet-inference-bootstrap/servicequotas.tf
@@ -1,5 +1,5 @@
 resource "aws_servicequotas_service_quota" "sagemaker_ml_g4dn_xlarge_for_endpoint_usage" {
   quota_code   = "L-B67CFA0C"
   service_code = "sagemaker"
-  value        = 7
+  value        = 6
 }
