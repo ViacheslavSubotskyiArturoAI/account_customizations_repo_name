db8b50f456efc1718da281dec76d4491ffcf1122
vsubotskyi <vsubotskyi@arturo.ai>
2024-10-18 16:34:37 +0300

    DEVOPSX-771: tune test account access to shared data

--- shared-data-us/terraform/locals.tf
+++ shared-data-us/terraform/locals.tf
@@ -3,7 +3,9 @@ locals {
   account_id = data.aws_caller_identity.this.account_id
   org_id     = data.aws_organizations_organization.this.id
 
-  org_root_id        = "r-gdnw"
-  org_ou_prod_id     = "ou-gdnw-j823h20x"
-  org_ou_non_prod_id = "ou-gdnw-6zk88t07"
+  org_root_id           = "r-gdnw"
+  org_ou_prod_id        = "ou-gdnw-j823h20x"
+  org_ou_prod_us_id     = "ou-gdnw-44rr102b"
+  org_ou_non_prod_id    = "ou-gdnw-6zk88t07"
+  org_ou_non_prod_us_id = "ou-gdnw-jjhvj9za"
 }
